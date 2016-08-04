a074a2292df6faeed064ddbbc90ae61dae0831ae
ringtail <zhongwei.lzw@alibaba-inc.com>
2016-08-04 11:41:01 +0800

    add new version

--- serviceA/app.py
+++ serviceA/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def world():
-        return 'world!'
+        return 'AliyunContainerService!'
 
 if __name__ == '__main__':
         app.run(port=5000,debug=True,host='0.0.0.0')
